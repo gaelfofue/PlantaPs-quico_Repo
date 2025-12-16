c5752fa775fadf9cf4177bc572630a853c463c64
gaelfofue <gaelfofue2006@gmail.com>
2025-12-16 12:24:35 +0100

    sad

--- PlantaPsíquico_Project/Assets/_PlantaPsíquico_ROOT/Scripts/PlayerController.cs
+++ PlantaPsíquico_Project/Assets/_PlantaPsíquico_ROOT/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using unityEngine.InputSystem;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
