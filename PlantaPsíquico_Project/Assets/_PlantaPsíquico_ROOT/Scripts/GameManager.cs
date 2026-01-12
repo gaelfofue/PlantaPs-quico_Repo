365446da10565172bff684e15f1d4ee69bb7e05f
gaelfofue <gaelfofue2006@gmail.com>
2026-01-12 16:49:01 +0100

    Game Manager

--- PlantaPsíquico_Project/Assets/_PlantaPsíquico_ROOT/Scripts/GameManager.cs
+++ PlantaPsíquico_Project/Assets/_PlantaPsíquico_ROOT/Scripts/GameManager.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private static GameManager _instance;
+    public static GameManager Instance
     {
-        
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.Log("Game Manager is null");
+            }
+
+            return _instance;
+        }
+    }
+    // Fin del "Singleton" básico, se referencia en el Awake
+
+    //Variables 
+    public int points;
+    public int winPoints;
+
+    private void Awake()
+    {
+       _instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
